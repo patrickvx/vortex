0590f65b237e209d57f62440e4c3ebc86240c138
patrickvx <patrickvx1@outlook.com>
2024-11-12 08:08:45 +0000

    Update keysystem.lua

--- keysystem.lua
+++ keysystem.lua
@@ -3,3 +3,8 @@ local keySystem = {}
 function keySystem:checkHWID(hwid : string)
 return true
 end
+
+function keySystem:copyLink(hwid : string)
+local url = ""
+  setclipboard(url .. "?hwid=" .. hwid)
+end
