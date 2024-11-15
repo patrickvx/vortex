7c9a7647a77b139e3e43e0ccc4066a481af8ce54
patrickvx <patrickvx1@outlook.com>
2024-11-15 15:39:43 +0000

    Update syntaxhighlighter.lua

--- syntaxhighlighter.lua
+++ syntaxhighlighter.lua
@@ -57,7 +57,7 @@ function syntaxHighlighter.highlight(source : string)
 			i = endPos + 2
 			
 		elseif source:sub(i, i):match("%d") or source:sub(i, i + 1):match("%.%d") then
-			local numEnd = source:find("%.") or source:find("[^%d%a%._]", i) or len + 1
+			local numEnd = source:find("%.") or source:find("[^%d%a_]", i) or len + 1
 			local number = source:sub(i, numEnd - 1)
 			table.insert(tokens, colorTag(colors["number"], number))
 			i = numEnd
